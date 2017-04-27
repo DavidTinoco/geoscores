8e146d6fe7dd1b827006aa7453da425d51cecdcc
DavidTinoco <daviid.tc@gmail.com>
2017-04-27 16:03:31 +0200

    Comentarios actualizados en programa principal

--- geoscores.py
+++ geoscores.py
@@ -6,6 +6,7 @@ from sys import argv
 
 futbolkey = os.environ["futbolkey"]
 
+#Página de inicio de la aplicación
 @route('/')
 def consulta():
     url_base = 'http://apiclient.resultados-futbol.com/scripts/api/api.php'
@@ -15,10 +16,12 @@ def consulta():
         doc = etree.fromstring(r.text.encode('utf-8'))
         return template("inicio.tpl", doc=doc)
 
+#Página dónde realizaremos la consulta de la liga y la jornada que queremos localizar
 @route('/localizalo')
 def peticion():
     return template("peticion.tpl")
 
+#Página dónde aparecerá la clasificación de primera división
 @route('/clasificacion/ligasantander')
 def clasificacion1():
     url_base = 'http://apiclient.resultados-futbol.com/scripts/api/api.php'
@@ -28,6 +31,7 @@ def clasificacion1():
         doc = etree.fromstring(r.text.encode('utf-8'))
         return template("clasificacion1.tpl",doc=doc)
 
+##Página dónde aparecerá la clasificación de segundo división
 @route('/clasificacion/liga123')
 def clasificacion2():
     url_base = 'http://apiclient.resultados-futbol.com/scripts/api/api.php'
@@ -37,14 +41,18 @@ def clasificacion2():
         doc = etree.fromstring(r.text.encode('utf-8'))
         return template("clasificacion2.tpl", doc=doc)
 
-
+#Página en la cual aparecerá la jornada Geolocalizada
 @post('/localizados')
 def resultado():
+    #Recibimos los parámetros liga y jornada que el usario desea geolocalizar
     liga = request.forms.get('liga')
     jornada = request.forms.get('jornada')
-    url_base_mapa="maps.google.com/maps/api/geocode/xml"
+    #Url's base de las apis a utilizar en ésta consulta
+    url_base_mapa="maps.google.com/maps/api/geocode/xml?address="
     url_base_futbol = "http://apiclient.resultados-futbol.com/scripts/api/api.php"
+    #Payload que necesitaremos para la consulta principal, que son los partidos de la jornada requerida por el usuario
     payload_futbol = {'key':futbolkey, 'format':'xml','req':'matchs','league':liga, 'round':jornada}
+    #Variables que vamos a necesitar para construir la página y el mapa
     latitud=[]
     longitud=[]
     local=[]
@@ -52,13 +60,19 @@ def resultado():
     fecha=[]
     hora=[]
     resultado=[]
+    #Realizamos la consulta a la api de resultados-fútbol
     r = requests.get(url_base_futbol, params=payload_futbol)
+    #Si no devuelve que ha encontrado lo solicitado
     if r.status_code == 200:
+        #Guardamos en doc la consulta para recorrer
         doc = etree.fromstring(r.text.encode('utf-8'))
+        #Vamos a necesitar el equipo local, visitante, la fecha y hora y el resultado
         for l,v,f,h,m,q in zip(doc.xpath("//local"),doc.xpath("//visitor"),doc.xpath("//date"),doc.xpath("//hour"),doc.xpath("//minute"),doc.xpath("//result")):
+            #Necesitamos las coordenadas del estadio, para ello haremos la consulta a Google Maps del Estadio "local"
             busqueda = 'Estadio+'+l.text.replace(" ","+")
-            s = requests.get(url_base_mapa+'?address='+busqueda)
+            s = requests.get(url_base_mapa+busqueda)
             doc2 = etree.fromstring(s.text.encode('utf-8'))
+            #Vamos almacenando en las listas los valores que luego utilizaremos
             latitud.append(doc2.xpath("//geometry/lat").text)
             longitud.append(doc2.xpath("//geometry/lng").text)
             local.append(l.text)
@@ -66,10 +80,10 @@ def resultado():
             fecha.append(f.text)
             hora.append(h.text+":"+m.text)
             resultado.append(q.text)
-            print latitud
-
+        #Le pasamos a la página los parámetro que va a necesitar para formarla    
         return template("localizados.tpl",lat=latitud, lng=longitud, local=local, visitante=visitante, fecha=fecha, hora=hora, resultado=resultado)
     else:
+        #Si no encontró lo solicitado, devolvemos un mensaje para que el usuario sea consciente de lo ocurrido.
         return template("jornadanotfound.tpl")
 
 @route('/static/<filepath:path>')
